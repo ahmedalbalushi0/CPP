6830e48c3822cd594f49ee3ac4a8f3bd455e8346
Ahmed <ahmed1balushi3@gmail.com>
2026-06-19 20:04:43 +0400

    Another number pattern

---
+++ patterns/numpat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int num;
+        for(int j=1;j<=i+1;j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
