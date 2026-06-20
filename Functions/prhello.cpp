8ccae9e071f27e7d73852e57ab7eecff30c0ce04
Ahmed <ahmed1balushi3@gmail.com>
2026-06-20 19:31:28 +0400

    void function

---
+++ Functions/prhello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//function definition
+void printHello(){
+    cout << "Hello\n";
+}
+int main(){
+    //function caal / invoke
+    printHello();
+    printHello();
+    return 0;
+}
