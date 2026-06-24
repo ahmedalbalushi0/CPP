1441c3ac0d6abdc1962d85f67413b37628606271
Ahmed <ahmed1balushi3@gmail.com>
2026-06-24 17:01:34 +0400

    loops on arrays

--- Arrays/student.cpp
+++ Arrays/student.cpp
@@ -2,13 +2,22 @@
 using namespace std;
 //New Chapter: Arrays
 int main(){
-    int mark[5] = {99, 100, 48, 93, 67}; //declaring an array with a size of 5 blocks
-    mark[0] = 89; 
-    cout << mark[0] << endl;
-    cout << mark[1] << endl;
-    cout << mark[2] << endl;
-    cout << mark[3] << endl;
-    cout << mark[4] << endl;
+    int marks[5] = {99, 100, 48, 93, 67}; //declaring an array with a size of 5 blocks
+    marks[0] = 89; 
+    //cout << marks[0] << endl;
+    //cout << marks[1] << endl;
+    //cout << marks[2] << endl;
+    //cout << marks[3] << endl;
+    //cout << marks[4] << endl;
+
+    // loops on Arrays
+    //size of an array: 
+    int size = sizeof(marks)/ sizeof(int);
+    cout <<"size of array marks: " << size << endl;
+
+    for(int i=0;i<=size;i++){
+        cout <<"index (" <<i << ") = "<< marks[i]<< endl;
+    }
 
     return 0;
 }
